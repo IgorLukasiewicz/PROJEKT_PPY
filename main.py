295a32f25b1185f7ae6cae14b62f828ce377a60a
ExtryDX <xavery.czuraj@gmail.com>
2025-05-31 17:13:27 +0200

    Add Game Loop, Event Loop, Assets (including some Sprites anc Collor Pallets) and Window Icon and text.

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
 import pygame
+from sys import exit
+
 
 width = 1280
 height = 720
 
 pygame.init()
+
 window = pygame.display.set_mode((width, height))
+pygame.display.set_caption('Echo Pets')
+
+window_icon = pygame.image.load('Assets\Images\Sprites\BushMonster.png')
+pygame.display.set_icon(window_icon)
+
+clock = pygame.time.Clock()
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
+            exit()
     pygame.display.update()
+    clock.tick(60)
 
