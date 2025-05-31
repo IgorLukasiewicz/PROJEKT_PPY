8e09f8882feb3433c36a78866a746521f9a5cc50
ExtryDX <xavery.czuraj@gmail.com>
2025-05-31 15:36:23 +0200

    Initial commit

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("Test")
+import pygame
+
+width = 1280
+height = 720
+
+pygame.init()
+window = pygame.display.set_mode((width, height))
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+    pygame.display.update()
+
